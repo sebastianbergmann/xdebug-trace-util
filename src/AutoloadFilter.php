554477eac907b17f46bcf59b19abe98ca850c1f4
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-05-01 08:57:22 +0200

    Initial work on filter for spl_autoload_call()

---
+++ src/AutoloadFilter.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/xdebug-trace-util.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\XdebugTraceUtil;
+
+final class AutoloadFilter extends \FilterIterator
+{
+    private bool $accept = true;
+
+    private int $level = 0;
+
+    public function __construct(FrameCollectionIterator $frames)
+    {
+        parent::__construct($frames);
+    }
+
+    public function accept(): bool
+    {
+        $frame = $this->getInnerIterator()->current();
+
+        \assert($frame instanceof Frame);
+
+        if ($this->accept && $frame->isEntry() && $frame->name() === 'spl_autoload_call') {
+            $this->accept = false;
+            $this->level  = $frame->level();
+
+            return false;
+        }
+
+        if (!$this->accept && $frame->isExit() && $frame->level() === $this->level) {
+            $this->accept = true;
+
+            return false;
+        }
+
+        return $this->accept;
+    }
+}
