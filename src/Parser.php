a6745696173931776c88c4bbc087a2d59a172424
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-05-01 08:53:41 +0200

    Initial work on trace file parser

---
+++ src/Parser.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/xdebug-trace-util.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\XdebugTraceUtil;
+
+final class Parser
+{
+    public function load(string $filename): FrameCollection
+    {
+        $file   = new \SplFileObject($filename);
+        $frames = [];
+
+        while (!$file->eof()) {
+            $line = $file->fgetcsv("\t");
+
+            if (!$line) {
+                break;
+            }
+
+            if (\count($line) === 1 && \strpos((string) $line[0], 'Version:') === 0) {
+                continue;
+            }
+
+            if (\count($line) === 1 && \strpos((string) $line[0], 'File format:') === 0) {
+                continue;
+            }
+
+            if (\count($line) === 1 && \strpos((string) $line[0], 'TRACE') === 0) {
+                continue;
+            }
+
+            if (\count($line) === 10 && $line[2] === '0') {
+                $frames[] = Frame::entry(
+                    (int) $line[0],
+                    (int) $line[1],
+                    (float) $line[3],
+                    (int) $line[4],
+                    (string) $line[5],
+                    $line[6] === '0',
+                    (string) $line[7],
+                    (string) $line[8],
+                    (int) $line[9]
+                );
+
+                continue;
+            }
+
+            if (\count($line) === 5 && $line[2] === '1') {
+                $frames[] = Frame::exit(
+                    (int) $line[0],
+                    (int) $line[1],
+                    (float) $line[3],
+                    (int) $line[4]
+                );
+
+                continue;
+            }
+
+            if (\count($line) === 6 && $line[2] === 'R') {
+                $frames[] = Frame::return(
+                    (int) $line[0],
+                    (int) $line[1],
+                    (string) $line[5]
+                );
+            }
+        }
+
+        return FrameCollection::fromList(...$frames);
+    }
+}
---
+++ tests/unit/ParserTest.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/xdebug-trace-util.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\XdebugTraceUtil;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \SebastianBergmann\XdebugTraceUtil\Parser
+ *
+ * @uses \SebastianBergmann\XdebugTraceUtil\EntryFrame
+ * @uses \SebastianBergmann\XdebugTraceUtil\ExitFrame
+ * @uses \SebastianBergmann\XdebugTraceUtil\Frame
+ * @uses \SebastianBergmann\XdebugTraceUtil\FrameCollection
+ * @uses \SebastianBergmann\XdebugTraceUtil\FrameWithTimeAndMemory
+ */
+final class ParserTest extends TestCase
+{
+    public function testParsesXdebugTraceFile(): void
+    {
+        $frames = (new Parser)->load(__DIR__ . '/../fixture/trace.xt')->asArray();
+
+        $this->assertCount(28, $frames);
+
+        $frame = $frames[0];
+        $this->assertTrue($frame->isEntry());
+        $this->assertSame('{main}', $frame->name());
+        $this->assertFalse($frame->isInternal());
+        $this->assertSame('', $frame->includeFile());
+        $this->assertSame('/home/sb/test.php', $frame->file());
+        $this->assertSame(0, $frame->line());
+        $this->assertSame(0.000126, $frame->time());
+        $this->assertSame(473248, $frame->memory());
+        $this->assertSame(1, $frame->level());
+        $this->assertSame(0, $frame->function());
+
+        $frame = $frames[1];
+        $this->assertTrue($frame->isEntry());
+        $this->assertSame('str_split', $frame->name());
+        $this->assertTrue($frame->isInternal());
+        $this->assertSame('', $frame->includeFile());
+        $this->assertSame('/home/sb/test.php', $frame->file());
+        $this->assertSame(8, $frame->line());
+        $this->assertSame(0.000145, $frame->time());
+        $this->assertSame(473248, $frame->memory());
+        $this->assertSame(2, $frame->level());
+        $this->assertSame(1, $frame->function());
+
+        $frame = $frames[2];
+        $this->assertTrue($frame->isExit());
+        $this->assertSame(0.000151, $frame->time());
+        $this->assertSame(473816, $frame->memory());
+        $this->assertSame(2, $frame->level());
+        $this->assertSame(1, $frame->function());
+
+        // How paranoid do I want to be?
+
+        $frame = $frames[27];
+        $this->assertTrue($frame->isExit());
+        $this->assertSame(0.000274, $frame->time());
+        $this->assertSame(473312, $frame->memory());
+        $this->assertSame(1, $frame->level());
+        $this->assertSame(0, $frame->function());
+    }
+}
